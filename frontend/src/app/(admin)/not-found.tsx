1d51ee72c0ccd06eb7d541ed28471998b2dd4aa2
Mikhail Florya <floria.mahail@gmail.com>
2025-12-25 21:15:39 +0300

    translate page error 404 alt

--- frontend/src/app/(admin)/not-found.tsx
+++ frontend/src/app/(admin)/not-found.tsx
@@ -6,7 +6,7 @@ import PageBreadcrumb from '@/components/layout/PageBreadcrumb'
 const NotFound = () => {
   return (
     <>
-      <PageBreadcrumb title="404" subName="Pages" />
+      <PageBreadcrumb title="404" subName="Страницы" />
       <Row className="justify-content-center">
         <Col xl={5}>
           <Card>
@@ -14,14 +14,14 @@ const NotFound = () => {
               <div className="p-4">
                 <div className="mx-auto mb-4 text-center">
                   <h1 className="mb-3 fw-bold fs-60">404</h1>
-                  <h2 className="fs-22 lh-base">Page Not Found !</h2>
+                  <h2 className="fs-22 lh-base">Страница не найдена!</h2>
                   <p className="text-muted mt-1 mb-4">
-                    The page you&apos;re trying to reach seems to have gone <br />
-                    missing in the digital wilderness.
+                    Страница, которую вы пытаетесь найти, похоже, потерялась <br />
+                    в цифровой пустыне.
                   </p>
                   <div className="text-center">
                     <Link to="/" className="btn btn-success">
-                      Back to Home
+                      Вернуться на главную
                     </Link>
                   </div>
                 </div>
