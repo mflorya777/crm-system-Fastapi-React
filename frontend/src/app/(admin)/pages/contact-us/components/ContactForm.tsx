81feb4384f7ee7c488c5c31f8b58b3ea6314e725
Mikhail Florya <floria.mahail@gmail.com>
2025-12-25 21:05:04 +0300

    translate page contact us

--- frontend/src/app/(admin)/pages/contact-us/components/ContactForm.tsx
+++ frontend/src/app/(admin)/pages/contact-us/components/ContactForm.tsx
@@ -9,10 +9,10 @@ import IconifyIcon from '@/components/wrappers/IconifyIcon'
 
 const ContactForm = () => {
   const contactFormSchema = yup.object({
-    fName: yup.string().required('please enter your first name'),
-    lName: yup.string().required('please enter your last name'),
-    email: yup.string().email('please enter valid email').required('please enter your last name'),
-    message: yup.string().required('please enter your message'),
+    fName: yup.string().required('Пожалуйста, введите ваше имя'),
+    lName: yup.string().required('Пожалуйста, введите вашу фамилию'),
+    email: yup.string().email('Пожалуйста, введите корректный email').required('Пожалуйста, введите ваш email'),
+    message: yup.string().required('Пожалуйста, введите ваше сообщение'),
   })
 
   const { control, handleSubmit } = useForm({
@@ -22,18 +22,18 @@ const ContactForm = () => {
   return (
     <Card className="mb-0 shadow-none border-0">
       <CardHeader className="bg-light-subtle border-0">
-        <h4 className="mb-0">Ask your different questions</h4>
+        <h4 className="mb-0">Задайте ваши вопросы</h4>
       </CardHeader>
       <CardBody>
         <form className="authentication-form" onSubmit={handleSubmit(() => {})}>
           <Row className="mb-3">
-            <TextFormInput name="fName" label="First Name" containerClassName="col-lg-6" placeholder="First name" control={control} />
-            <TextFormInput name="lName" label="Last Name" containerClassName="col-lg-6" placeholder="Last name" control={control} />
+            <TextFormInput name="fName" label="Имя" containerClassName="col-lg-6" placeholder="Имя" control={control} />
+            <TextFormInput name="lName" label="Фамилия" containerClassName="col-lg-6" placeholder="Фамилия" control={control} />
           </Row>
-          <TextFormInput name="email" type="email" label="Email" containerClassName="mb-3" placeholder="Enter your email" control={control} />
+          <TextFormInput name="email" type="email" label="Email" containerClassName="mb-3" placeholder="Введите ваш email" control={control} />
           <div className="mb-3">
             <label className="form-label" htmlFor="contactnumber">
-              Contact Number
+              Номер телефона
             </label>
             <Dropdown className="form-group input-group">
               <DropdownToggle className="btn btn-light rounded-end-0 border arrow-none" type="button">
@@ -61,25 +61,25 @@ const ContactForm = () => {
               <input type="number" className="form-control" id="contactnumber" placeholder="+0(222)000-0000" />
             </Dropdown>
           </div>
-          <TextAreaFormInput name="message" label="Message" control={control} rows={5} containerClassName="mb-3" placeholder="Max 150 words" />
+          <TextAreaFormInput name="message" label="Сообщение" control={control} rows={5} containerClassName="mb-3" placeholder="Максимум 150 слов" />
           <div className="mb-3">
-            <h5 className="my-3">Services</h5>
+            <h5 className="my-3">Услуги</h5>
             <Row>
               <Col xxl={6}>
-                <FormCheck label="Website Design" id="check1" className="mb-2" />
-                <FormCheck label="UX Design" id="check2" className="mb-2" />
-                <FormCheck label="User Research" id="check3" className="mb-2" />
+                <FormCheck label="Дизайн веб-сайта" id="check1" className="mb-2" />
+                <FormCheck label="UX дизайн" id="check2" className="mb-2" />
+                <FormCheck label="Исследование пользователей" id="check3" className="mb-2" />
               </Col>
               <Col xxl={6}>
-                <FormCheck label="Content Creation" id="check4" className="mb-2" />
-                <FormCheck label="Strategy & Consulting" id="check5" className="mb-2" />
-                <FormCheck label="Other" id="check6" className="mb-2" />
+                <FormCheck label="Создание контента" id="check4" className="mb-2" />
+                <FormCheck label="Стратегия и консультирование" id="check5" className="mb-2" />
+                <FormCheck label="Другое" id="check6" className="mb-2" />
               </Col>
             </Row>
           </div>
           <div className="text-center d-grid">
             <Button variant="primary" type="submit">
-              Send Message
+              Отправить сообщение
             </Button>
           </div>
         </form>
