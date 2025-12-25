1569199076ac0c36de67ccf04eaeb4dcf8f30e99
Mikhail Florya <floria.mahail@gmail.com>
2025-12-25 21:13:47 +0300

    translate page error 404

--- frontend/src/app/(other)/(error-pages)/error-404/page.tsx
+++ frontend/src/app/(other)/(error-pages)/error-404/page.tsx
@@ -18,15 +18,15 @@ const Error404 = () => {
               <div className="mx-auto mb-4 text-center">
                 <LogoBox containerClassName="mx-auto text-center auth-logo" textLogo={{ height: 24, width: 72 }} squareLogo={{ className: 'me-1' }} />
                 <h1 className="mt-5 mb-3 fw-bold fs-60">404</h1>
-                <h2 className="fs-22 lh-base">Page Not Found !</h2>
+                <h2 className="fs-22 lh-base">Страница не найдена!</h2>
                 <p className="text-muted mt-1 mb-4">
-                  The page you&apos;re trying to reach seems to have gone
+                  Страница, которую вы пытаетесь найти, похоже, потерялась
                   <br />
-                  missing in the digital wilderness.
+                  в цифровой пустыне.
                 </p>
                 <div className="text-center">
                   <Link to="/" className="btn btn-success">
-                    Back to Home
+                    Вернуться на главную
                   </Link>
                 </div>
               </div>
