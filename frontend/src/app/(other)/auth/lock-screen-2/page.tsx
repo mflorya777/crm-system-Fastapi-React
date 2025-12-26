4a8cd3e8aab0e2442d02a9c990e175ee6a21ebec
Mikhail Florya <floria.mahail@gmail.com>
2025-12-26 10:22:13 +0300

    translate page lock screen 2

--- frontend/src/app/(other)/auth/lock-screen-2/page.tsx
+++ frontend/src/app/(other)/auth/lock-screen-2/page.tsx
@@ -8,7 +8,7 @@ import LockScreenForm from './components/LockScreenForm'
 const LockScreen2 = () => {
   return (
     <>
-      <PageMetaData title="Lock Screen 2" />
+      <PageMetaData title="Блокировка экрана 2" />
 
       <Col xl={5} className="mx-auto">
         <Card className="auth-card">
@@ -21,17 +21,17 @@ const LockScreen2 = () => {
               squareLogo={{ className: 'me-1' }}
               containerClassName="mx-auto mb-4 text-center auth-logo"
             />
-            <h2 className="fw-bold text-center fs-18">Hi ! Gaston</h2>
-            <p className="text-muted text-center mt-1 mb-4">Enter your password to access the admin.</p>
+            <h2 className="fw-bold text-center fs-18">Привет! Gaston</h2>
+            <p className="text-muted text-center mt-1 mb-4">Введите ваш пароль для доступа к админ-панели.</p>
             <div className="px-4">
               <LockScreenForm />
             </div>
           </CardBody>
         </Card>
         <p className="text-white mb-0 text-center">
-          Not you? return{' '}
+          Не вы? вернуться{' '}
           <Link to="/auth/sign-in-2" className="text-white fw-bold ms-1">
-            Sign In
+            Войти
           </Link>
         </p>
       </Col>
