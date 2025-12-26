06cdbffb5efc012bfba7f101f3cffcb01e3c1b78
Mikhail Florya <floria.mahail@gmail.com>
2025-12-26 10:27:58 +0300

    translate footer & header

--- frontend/src/context/constants.ts
+++ frontend/src/context/constants.ts
@@ -2,18 +2,18 @@ type CurrencyType = '₹' | '$' | '€'
 
 export const currency: CurrencyType = '$'
 
-export const currentYear = new Date().getFullYear()
+export const currentYear = 2026
 
 export const developedByLink = 'https://techzaa.getappui.com/'
 
-export const developedBy = 'Techzaa'
+export const developedBy = 'guki_lovely'
 
-export const contactUs = 'techzaastudio@gmail.com'
+export const contactUs = 'mflorya@gmail.com'
 
 export const buyLink = ''
 
 export const basePath = ''
 
-export const DEFAULT_PAGE_TITLE = 'Reback React | Responsive Admin Dashboard Template'
+export const DEFAULT_PAGE_TITLE = 'PinscherCRM React | Responsive Admin Dashboard Template'
 
 export const colorVariants = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark', 'purple', 'pink', 'orange', 'light', 'link']
